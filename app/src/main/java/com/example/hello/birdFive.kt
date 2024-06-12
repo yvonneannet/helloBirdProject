3f1ba8fe5b8ca218dcd8bdd30299d3676630302c
yvonneoyella <yvonneoyellaa@gmail.com>
2024-06-12 22:17:26 +0300

    added image

--- app/src/main/java/com/example/hello/birdFive.kt
+++ app/src/main/java/com/example/hello/birdFive.kt
@@ -4,20 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.example.hello.databinding.ActivityBirdFiveBinding
+import com.example.hello.databinding.ActivityBirdTwoBinding
+import com.squareup.picasso.Picasso
+
 
 class birdFive : AppCompatActivity() {
+    lateinit var binding: ActivityBirdFiveBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bird_five)
+        setContentView(binding.root)
 
 
-        val btnback4 =findViewById<ImageView>(R.id.btnback4)
 
-
-        btnback4.setOnClickListener{
+        binding.btnback4.setOnClickListener{
             finish()
         }
 
-
+        Picasso.get().load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnRGThSt8eU-kUDtaUEZIWtc8KACH5OdmZ_w&s").into(binding.imageView5)
     }
 }
--- app/src/main/java/com/example/hello/birdTwo.kt
+++ app/src/main/java/com/example/hello/birdTwo.kt
@@ -4,21 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.example.hello.databinding.ActivityBirdTwoBinding
 import com.example.hello.databinding.ActivityMainBinding
 import com.squareup.picasso.Picasso
 
 class BirdTwo : AppCompatActivity() {
 
 
-lateinit var binding: ActivityMainBinding
+lateinit var binding: ActivityBirdTwoBinding
 
 override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    binding= ActivityMainBinding.inflate(layoutInflater)
+    binding= ActivityBirdTwoBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
 
-    binding.btnNext.setOnClickListener{
+    binding.btnNext2.setOnClickListener{
         val intent = Intent(this, BirdThree::class.java)
         startActivity(intent)
 
@@ -26,7 +27,7 @@ override fun onCreate(savedInstanceState: Bundle?) {
 
 
         }
-    Picasso.get().load("https://images.unsplash.com/photo-1574068468668-a05a11f871da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGJpcmRzfGVufDB8fDB8fHww").into(binding.imageView2)
+    Picasso.get().load("https://images.unsplash.com/photo-1574068468668-a05a11f871da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGJpcmRzfGVufDB8fDB8fHww").into(binding.imageView)
 }
 }
 
